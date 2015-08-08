a2db3d0062aa01dd57ec1329fbb80839e00a065e
Stephen Vavasis <vavasis@uwaterloo.ca>
2015-08-08 14:58:27 -0400

    Tests do not use tuple any more for 0.3 compatibility

--- test/test_unroll.jl
+++ test/test_unroll.jl
@@ -4,18 +4,18 @@ using Unroll.@unroll
 const INNERLOOPBOUND = 2
 
 function tu1_plain(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        for i = 1 : n
+        for i = 1 : n - 2
             for j = 1 : 2
-                x += a[i][j] + b[i][j]
+                x += a[i] * b[i+j]
             end
         end
         y += x
@@ -24,42 +24,37 @@ function tu1_plain(n)
 end
 
 function tu1_handunroll(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        for i = 1 : n
-            x += a[i][1] + b[i][1]
-            x += a[i][2] + b[i][2]
+        for i = 1 : n - 2
+            x += a[i] * b[i+1]
+            x += a[i] * b[i+2]
         end
         y += x
     end
     y
 end
 
-
-
 function tu1_macrounroll(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        #println(macroexpand(:(@unroll for j = 1 : 2
-        #        x += a[i][j] + b[i][j]
-        #        end)))
-        for i = 1 : n
+        for i = 1 : n - 2
             @unroll for j = 1 : 2
-                x += a[i][j] + b[i][j]
+                x += a[i] * b[i+j]
             end
         end
         y += x
@@ -69,21 +64,20 @@ end
 
 
 
-
 function tu2_plain(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        for i = 1 : n
-            for j = 1 : INNERLOOPBOUND
-                for k = 1 : INNERLOOPBOUND
-                    x += a[i][j] + b[i][k]
+        for i = 1 : n - 2
+            for j = 1 : 2
+                for k = 1 : 2
+                    x += a[i+k] * b[i+j]
                 end
             end
         end
@@ -93,20 +87,42 @@ function tu2_plain(n)
 end
 
 function tu2_handunroll(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        for i = 1 : n
-            x += a[i][1] + b[i][1]
-            x += a[i][1] + b[i][2]
-            x += a[i][2] + b[i][1]
-            x += a[i][2] + b[i][2]
+        for i = 1 : n - 2
+            x += a[i+1] * b[i+1]
+            x += a[i+2] * b[i+1]
+            x += a[i+1] * b[i+2]
+            x += a[i+2] * b[i+2]
+        end
+        y += x
+    end
+    y
+end
+
+function tu2_macrounroll(n)
+    a = Array{Int,1}()
+    b = Array{Int,1}()
+    for i = 1 : n
+        push!(a, i)
+        push!(b, 2*i)
+    end
+    y = 1
+    for tr = 1 : 10
+        x = 0
+        for i = 1 : n - 2
+            @unroll for j = 1 : 2
+                @unroll for k = 1 : 2
+                    x += a[i+k] * b[i+j]
+                end
+            end
         end
         y += x
     end
@@ -114,45 +130,22 @@ function tu2_handunroll(n)
 end
 
 
-function tu2_macrounroll(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
-    for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
-    end
-    y = 1
-    for tr = 1 : 10
-        x = 0
-        for i = 1 : n
-           @unroll for j = 1 : INNERLOOPBOUND
-                @unroll for k = 1 : INNERLOOPBOUND
-                    x += a[i][j] + b[i][k]
-                end
-            end
-         end
-        y += x
-    end
-    y
-end
-
-
 function tu3_plain(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        for i = 1 : n
+        for i = 1 : n - 2
             for j = 1 : 2
-                if mod(j,2) == 1
-                    x += a[i][j] + b[i][j]
+                if j == 1
+                    x += a[i] * b[i+j]
                 else
-                    x += a[i][j] + 2 * b[i][j]
+                    x -= a[i] * b[i+j]
                 end
             end
         end
@@ -163,40 +156,41 @@ end
 
 
 function tu3_handunroll(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        for i = 1 : n
-            x += a[i][1] + b[i][1]
-            x += a[i][2] + 2 * b[i][2]
+        for i = 1 : n - 2
+            x += a[i] * b[i+1]
+            x -= a[i] * b[i+2]
         end
         y += x
     end
     y
 end
 
+
 function tu3_macrounroll(n)
-    a = Array{Tuple{Int,Int},1}()
-    b = Array{Tuple{Int,Int},1}()
+    a = Array{Int,1}()
+    b = Array{Int,1}()
     for i = 1 : n
-        push!(a, (i,i))
-        push!(b, (2*i,2*i))
+        push!(a, i)
+        push!(b, 2*i)
     end
     y = 1
     for tr = 1 : 10
         x = 0
-        for i = 1 : n
+        for i = 1 : n - 2
             @unroll for j = 1 : 2
-                if mod(j,2) == 1
-                    x += a[i][j] + b[i][j]
+                if j == 1
+                    x += a[i] * b[i+j]
                 else
-                    x += a[i][j] + 2 * b[i][j]
+                    x -= a[i] * b[i+j]
                 end
             end
         end
