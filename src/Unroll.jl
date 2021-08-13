9d9d40a7a86c3fd5f10fae4334a9d2e0cfe15f40
Pepijn de Vos <pepijndevos@gmail.com>
2021-08-13 14:25:09 +0200

    evaluate into caller module

--- src/Unroll.jl
+++ src/Unroll.jl
@@ -1,20 +1,20 @@
 module Unroll
 
 
-copy_and_substitute_tree(e, varname, newtext) = e
+copy_and_substitute_tree(e, varname, newtext, mod) = e
 
-copy_and_substitute_tree(e::Symbol, varname, newtext) = 
+copy_and_substitute_tree(e::Symbol, varname, newtext, mod) =
     e == varname ? newtext : e
 
-function copy_and_substitute_tree(e::Expr, varname, newtext)
+function copy_and_substitute_tree(e::Expr, varname, newtext, mod)
     e2 = Expr(e.head)
     for subexp in e.args
-        push!(e2.args, copy_and_substitute_tree(subexp, varname, newtext))
+        push!(e2.args, copy_and_substitute_tree(subexp, varname, newtext, mod))
     end
     if e.head == :if
         newe = e2
         try
-            u = Core.eval(@__MODULE__, e2.args[1])
+            u = Core.eval(mod, e2.args[1])
             if u == true
                 newe = e2.args[2]
             elseif u == false
@@ -41,8 +41,8 @@ macro unroll(expr)
     end
     varname = expr.args[1].args[1]
     ret = Expr(:block)
-    for k in Core.eval(@__MODULE__, expr.args[1].args[2])
-        e2 = copy_and_substitute_tree(expr.args[2], varname, k)
+    for k in Core.eval(__module__, expr.args[1].args[2])
+        e2 = copy_and_substitute_tree(expr.args[2], varname, k, __module__)
         push!(ret.args, e2)
     end
     esc(ret)
@@ -56,8 +56,8 @@ macro tuplegen(expr)
     end
     varname = expr.args[2].args[1]
     ret = Expr(:tuple)
-    for k in Core.eval(@__MODULE__, expr.args[2].args[2])
-        e2 = copy_and_substitute_tree(expr.args[1], varname, k)
+    for k in Core.eval(__module__, expr.args[2].args[2])
+        e2 = copy_and_substitute_tree(expr.args[1], varname, k, __module__)
         push!(ret.args,e2)
     end
     esc(ret)
